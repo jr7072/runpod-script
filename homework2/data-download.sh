a5e27b7bc54ebf16dad9be05769f986f6ef09f93
Juan Rosales <juanrosales@Juans-MacBook-Pro.local>
2026-02-16 14:17:55 -0600

    modified data script

--- homework2/data-download.sh
+++ homework2/data-download.sh
@@ -1,11 +1,5 @@
 # install unzip
 apt update && sudo apt install unzip
 
-# download the classification data
-wget https://www.cs.utexas.edu/~bzhou/dl_class/classification_data.zip
-
 # unzip it
 unzip ./classification_data.zip
-
-# delete the zip file
-rm ./classification_data.zip
